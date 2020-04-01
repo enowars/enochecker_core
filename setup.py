aeac7a669ad0275c5fe380d5873d81ae74890cb5
Benedikt Radtke <Trolldemorted@users.noreply.github.com>
2020-04-01 21:43:56 +0200

    Bump version to 0.1.2

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("requirements.txt", "r") as f:
 
 setuptools.setup(
     name="enochecker_core",
-    version="0.1.1",
+    version="0.1.2",
     author="Trolldemorted",
     author_email="benediktradtke@gmail.com",
     description="Base library for enochecker libs",
